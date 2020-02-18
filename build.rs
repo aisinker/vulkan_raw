940b42bcd74262cfd80858cf8ea41bbcb5409e61
aisinker <aisinker@outlook.com>
2020-02-19 00:43:59 +0800

    Resolve docs.rs building documents failed problem.

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+#[cfg(feature = "docs")]
+fn main(){}
+
+#[cfg(not(feature = "docs"))]
 fn main(){
     let path = std::env::var("VK_SDK_PATH").expect("error!"); // or VULKAN_SDK
     println!("cargo:rustc-link-search=native={}{}Lib", path, std::path::MAIN_SEPARATOR);
